a9d3788e684ccd9088cf0e3a3b9d6a4c269f278b
Nick Doiron <ndoiron@mapmeld.com>
2016-09-04 15:33:39 -0500

    make the connection outside of the invoke

--- handler.js
+++ handler.js
@@ -1,14 +1,16 @@
 'use strict';
 
 var neo4j = require('neo4j');
+var db = new neo4j.GraphDatabase('http://app54430255-PFYkCV:TucLNepPzfueVUc2cnhE@app54430255pfykcv.sb10.stations.graphenedb.com:24789');
 
 // Your first function handler
 module.exports.hello = (event, context, cb) => {
-  var db = new neo4j.GraphDatabase('http://app54430255-PFYkCV:TucLNepPzfueVUc2cnhE@app54430255pfykcv.sb10.stations.graphenedb.com:24789');
   db.cypher({
     query: 'MATCH (a:Artist) RETURN COUNT(a) AS count'
   }, function(err, responses) {
-    cb(null, { message: 'Currently stores ' + responses[0].count + ' artists', event });
+    cb(null, {
+      message: 'Currently stores ' + responses[0].count + ' artists', event
+    });
   });
 };
 
