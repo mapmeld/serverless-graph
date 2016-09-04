a05f03c832ffc48e4a53fb095c3adba1feead821
Nick Doiron <ndoiron@mapmeld.com>
2016-09-04 15:08:39 -0500

    neo4j count test

--- handler.js
+++ handler.js
@@ -1,8 +1,15 @@
 'use strict';
 
+var neo4j = require('neo4j');
+
 // Your first function handler
-module.exports.hello = (event, context, cb) => cb(null,
-  { message: 'Go Serverless v1.0! Your function executed successfully!', event }
-);
+module.exports.hello = (event, context, cb) => {
+  var db = new neo4j.GraphDatabase('http://app54430255-PFYkCV:TucLNepPzfueVUc2cnhE@app54430255pfykcv.sb10.stations.graphenedb.com:24789');
+  db.cypher({
+    query: 'MATCH (a:Artist) RETURN COUNT(a) AS count'
+  }, function(err, responses) {
+    cb(null, { message: 'Currently stores ' + responses[0].count + ' artists', event });
+  });
+};
 
 // You can add more handlers here, and reference them in serverless.yml
